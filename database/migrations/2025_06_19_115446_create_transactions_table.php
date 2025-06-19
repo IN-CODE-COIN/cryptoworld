8fe5447ce392f773d40dec01a8437442ea4a8523
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-06-19 13:57:40 +0200

    <feat> add tables and migrations

---
+++ database/migrations/2025_06_19_115446_create_transactions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('wallet_id')->constrained()->onDelete('cascade');
+            $table->string('symbol', 10);
+            $table->enum('type', ['buy', 'sell']);
+            $table->decimal('price_usd', 18, 8);
+            $table->decimal('quantity', 18, 8);
+            $table->timestamp('date')->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
