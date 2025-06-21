d494c7234d61358bc7778cd05d0ccdb08c3978b9
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-06-21 14:31:32 +0200

    <feat> add logo cryptoworld svg

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,9 +1,11 @@
 <img
-    src="{{ asset('images/logo.webp') }}"
+    src="{{ asset('images/logo.svg') }}"
     alt="Logo Cryptoworld"
     width="128"
     height="48"
     class="w-32 h-auto object-contain"
     loading="eager"
 />
+
+
 
