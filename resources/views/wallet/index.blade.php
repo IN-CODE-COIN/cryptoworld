98575e5e57e6170ed31b06323308863fdf50ab83
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-06-23 18:43:42 +0200

    <fix> change all button return and add svg and span

--- resources/views/wallet/index.blade.php
+++ resources/views/wallet/index.blade.php
@@ -4,7 +4,10 @@
             <h2 class="font-semibold text-3xl text-gray-800 leading-tight">Cartera</h2>
             <a href="{{ route('home') }}"
                class="inline-flex items-center px-4 py-2 bg-gray-200 hover:bg-gray-300 text-gray-800 rounded-md text-xs font-medium">
-                ← Volver
+                <svg class="w-5 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14M5 12l4-4m-4 4 4 4"/>
+                </svg>
+                <span>Volver</span>
             </a>
         </div>
     </x-slot>
